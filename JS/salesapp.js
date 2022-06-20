c6c17e2352a28b1deb95e7f6bdf1f699f73af04b
Matthew Gebhart <Matthewgebhart@gmail.com>
2022-06-19 23:51:29 -0700

    added final touches and fonts

--- JS/salesapp.js
+++ JS/salesapp.js
@@ -1,81 +1,53 @@
 'use strict';
 
-let openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+let totalSalesArray = [];
 
-let dailyTotalArray = [];
 
-function custPerHour(max, min, avg, open = 6, close = 19) {
-  let hourlyAvg = [];
-  let hoursOpen = close - open;
-  let hour = open;
-  let timeOfDay = 'am';
-  let rand = 0;
-  let total = 0;
-  for (let i=0; i <= hoursOpen; i++) {
-    rand = Math.round(Math.round(Math.random() * (max - min) + min) * avg);
-    let salesPerHour = `${hour} ${timeOfDay}: ${rand}`;
-    hourlyAvg.push(rand);
-    hour++;
-    if (dailyTotalArray[i] == undefined) {
-      dailyTotalArray.push(rand);
-    }
-    else {
-      dailyTotalArray[i] += rand;
-    }
-    total += rand;
-  }
-  return [hourlyAvg, total];
+function generateNumberBetween(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
-
-function Store(name, min, max, avg, open = 6, close = 19) {
+function Store(name, minCust, maxCust, avgCookiePerCust) {
   this.name = name;
-  this.min = min;
-  this.max - max;
-  this.avg = avg;
-  this.custAtEachHourArray = custPerHour(max, min, avg)[0];
-  this.dailyTotal = custPerHour(max, min, avg)[1];
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.hourlySales = [];
+  this.totalSales = 0;
+  this.avgCookiePerCust = avgCookiePerCust;
 }
 
-// console.log(Store);
+Store.prototype.generateCookiesPerHour = function() {
+  for (let i = 0; i < hours.length; i++) {
+    let customersAtHour = generateNumberBetween(this.minCust, this.maxCust);
+    let salesAtHour = Math.round(customersAtHour * this.avgCookiePerCust);
+    this.hourlySales.push(salesAtHour);
+    this.totalSales += salesAtHour;
+    totalSalesArray.push(this.totalSales);
+  }
+};
 
-const seattle = new Store('seattle', 23, 65, 6.3);
-const tokyo = new Store('tokyo', 3, 24, 1.2);
-const dubai = new Store('dubai', 11, 38, 3.7);
-const paris = new Store('paris', 20, 38, 2.3);
-const lima = new Store('lima', 2, 16, 4.6);
-
-
-// console.log(seattle);
-// console.log(tokyo);
-// console.log(dubai);
-// console.log(paris);
-// console.log(lima);
-console.log(dailyTotalArray);
-
-
-
-/////// Write Table to page ////////
+///render hours header
 
 function renderHeader() {
-  let tableEl = document.getElementById('results-data');
+  let tableEl = document.getElementById('sales-data');
   let tableRowEl = document.createElement('tr');
   let startEl = document.createElement('td');
   let endEl = document.createElement('td');
   startEl.classList.add('empty');
   endEl.classList.add('total');
-
+  
   tableEl.appendChild(tableRowEl);
   tableRowEl.appendChild(startEl);
   startEl.textContent = '';
-
-  for (let i=0; i<openHours.length; i++) {
+  
+  for (let i=0; i<hours.length; i++) {
     let newDataCell = document.createElement('td');
     newDataCell.classList.add('heading');
     tableRowEl.appendChild(newDataCell);
-    newDataCell.textContent = openHours[i];
+    newDataCell.textContent = hours[i];
   }
-
+  
   tableEl.appendChild(tableRowEl);
   tableRowEl.appendChild(endEl);
   endEl.textContent = 'Total:';
@@ -83,163 +55,86 @@ function renderHeader() {
 
 renderHeader();
 
-function render(storeLocation) {
-  let tableEl = document.getElementById('results-data');
-  let tableRowEl = document.createElement('tr');
-  let storeEl = document.createElement('td');
-  storeEl.classList.add('store-location');
-  let totalEl = document.createElement('td');
-  totalEl.classList.add('totals');
+Store.prototype.renderData = function() {
+  let tableEl = document.getElementById('sales-data');
+  let rowEl = document.createElement('tr');
+  let nameCell = document.createElement('td');
+  nameCell.textContent = this.name;
+  rowEl.appendChild(nameCell);
 
-  tableEl.appendChild(tableRowEl);
-  tableRowEl.appendChild(storeEl);
-  storeEl.textContent = storeLocation.name;
-
-  console.log(storeLocation);
-
-  for (let i=0; i < storeLocation.custAtEachHourArray.length; i++) {
-    let newDataCell = document.createElement('td');
-    tableRowEl.appendChild(newDataCell);
-    newDataCell.textContent = storeLocation.custAtEachHourArray[i];
+  for (let i = 0; i < this.hourlySales.length; i++) {
+    let cellEl = document.createElement('td');
+    cellEl.textContent = this.hourlySales[i];
+    rowEl.appendChild(cellEl);
   }
 
-  tableRowEl.appendChild(totalEl);
-  totalEl.textContent = storeLocation.total;
+  let totalCell = document.createElement('td');
+  totalCell.textContent = this.totalSales;
+  rowEl.appendChild(totalCell);
+  tableEl.appendChild(rowEl);
+};
 
-}
+let seattle = new Store('Seattle', 10, 20, 3);
+let lima = new Store('Lima', 1, 50, 2.6);
+let paris = new Store('Paris', 5, 25, 4);
+let london = new Store('London', 13, 19, 6);
+let newYork = new Store('New York', 2, 15, 3.5);
 
-render(seattle);
-render(tokyo);
-render(dubai);
-render(paris);
-render(lima);
+seattle.generateCookiesPerHour();
+lima.generateCookiesPerHour();
+paris.generateCookiesPerHour();
+london.generateCookiesPerHour();
+newYork.generateCookiesPerHour();
+
+seattle.renderData();
+lima.renderData();
+paris.renderData();
+london.renderData();
+newYork.renderData();
 
 function renderFooter() {
-  let tableEl = document.getElementById('results-data');
+  let tableEl = document.getElementById('sales-data');
   let tableRowEl = document.createElement('tr');
   let startEl = document.createElement('td');
   let endEl = document.createElement('td');
   startEl.classList.add('total');
   endEl.classList.add('total');
-
+  
   tableEl.appendChild(tableRowEl);
   tableRowEl.appendChild(startEl);
   startEl.textContent = 'Total:';
-
+  
   let totalTotal = 0;
-
-  for ( let i=0; i<openHours.length; i++) {
+  
+  for ( let i=0; i<hours.length; i++) {
     let newDataCell = document.createElement('td');
     newDataCell.classList.add('total');
     tableRowEl.appendChild(newDataCell);
-    newDataCell.textContent = dailyTotalArray[i];
-    totalTotal += dailyTotalArray[i];
+    newDataCell.textContent = totalSalesArray[i];
+    totalTotal += totalSalesArray[i];
   }
-
+  
   tableEl.appendChild(tableRowEl);
   tableRowEl.appendChild(endEl);
   endEl.textContent = totalTotal;
 }
 
+
 renderFooter();
 
 function clearTable() {
-  let tableElement = document.getElementById('results-data');
+  let tableElement = document.getElementById('sales-data');
   tableElement.innerHTML = '';
-
 };
 
-///Collaboration credit to Isaiah Keller for helping me clean up some of the rendering stuff///
-
-
-
-///Adding Form input///
-
-let formElement = document.getElementById('new-store');
-// 
-
-
-let newStore = [];
-
-// function NewStore(name, max, min, avg) {
-//   this.storeName = name;
-//   this.maxCust = max;
-//   this.minCust = min;
-//   this.avgCookiesPerSale = avg;
-// }
-
-formElement.addEventListener('submit', function(event) {
+let storeFormEl = document.getElementById('store-form');
+storeFormEl.addEventListener('submit', function(event) {
   event.preventDefault();
-  console.log(event.target.storeName.value);
-  console.log(event.target.maxCust.value);
-  console.log(event.target.minCust.value);
-  console.log(event.target.avgSale.value);
-
   let name = event.target.storeName.value;
-  let max = event.target.maxCust.value;
-  let min = event.target.minCust.value;
-  let avg = event.target.avgSale.value;
-
-  let newStore = new Store(name, min, max, avg);
-
-  // newStore.push(store);
-  // console.log(newStore);
-  renderNewStore(newStore);
-  // return (newStore);
+  let minCust = parseInt(event.target.minCust.value);
+  let maxCust = parseInt(event.target.maxCust.value);
+  let avgCookies = parseInt(event.target.avgSale.value);
+  let store = new Store(name, minCust, maxCust, avgCookies);
+  store.generateCookiesPerHour();
+  store.renderData();
 });
-
-
-
-
-
-
-function renderNewStore(newStore) {
-  let tableEl = document.getElementById('results-data');
-  let tableRowEl = document.createElement('tr');
-  let storeEl = document.createElement('td');
-  storeEl.classList.add('store-location');
-  let totalEl = document.createElement('td');
-  totalEl.classList.add('totals');
-
-  tableEl.appendChild(tableRowEl);
-  tableRowEl.appendChild(storeEl);
-  storeEl.textContent = newStore.name;
-
-  console.log(newStore);
-
-  for (let i=0; i < newStore.custAtEachHourArray.length; i++) {
-    let newDataCell = document.createElement('td');
-    tableRowEl.appendChild(newDataCell);
-    newDataCell.textContent = newStore.custAtEachHourArray[i];
-  }
-
-  tableRowEl.appendChild(totalEl);
-  totalEl.textContent = newStore.total;
-
-}
-
-
-// function render(storeLocation) {
-//   let tableEl = document.getElementById('results-data');
-//   let tableRowEl = document.createElement('tr');
-//   let storeEl = document.createElement('td');
-//   storeEl.classList.add('store-location');
-//   let totalEl = document.createElement('td');
-//   totalEl.classList.add('totals');
-
-//   tableEl.appendChild(tableRowEl);
-//   tableRowEl.appendChild(storeEl);
-//   storeEl.textContent = storeLocation.name;
-
-//   console.log(storeLocation);
-
-//   for (let i=0; i < storeLocation.custAtEachHourArray.length; i++) {
-//     let newDataCell = document.createElement('td');
-//     tableRowEl.appendChild(newDataCell);
-//     newDataCell.textContent = storeLocation.custAtEachHourArray[i];
-//   }
-
-//   tableRowEl.appendChild(totalEl);
-//   totalEl.textContent = storeLocation.total;
-
-// }
